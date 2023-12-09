077020f30345b9e32d7f7459b72a9f1f75696003
Aaron Bentley <aaron@aaronbentley.com>
2023-12-09 14:31:30 -0500

    Shove the unsafe stuff into a sys module.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,21 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
+
+pub mod sys {
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+}
+
+unsafe extern "C" fn imu_callback (_: *mut u8, _: u16, _: u32) {
+}
+
+unsafe extern "C" fn mcu_callback (_: u16, _: *mut u8, _: u16, _: u32) {
+}
+
+
+
+pub fn safe_init() -> bool {
+    use self::sys::init;
+    unsafe {
+        init(Some(imu_callback), Some(mcu_callback))
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,5 @@
-use viture_rs::init;
-
-unsafe extern "C" fn imu_callback (_: *mut u8, _: u16, _: u32) {
-}
-
-unsafe extern "C" fn mcu_callback (_: u16, _: *mut u8, _: u16, _: u32) {
-}
-
+use viture_rs::safe_init;
 
 fn main() {
-    println!("Hello, world!");
-    unsafe {
-        println!("{}", init(Some(imu_callback), Some(mcu_callback)));
-    }
+    println!("{}", safe_init());
 }
